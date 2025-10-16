f8878bce4a656f142aab124f09371229bf392b97
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-10-16 09:17:37 +0300

    Add gcd and lcm functions

---
+++ Lab/Lab 5/src/main/java/MathUtils.java
@@ -0,0 +1,15 @@
+public class MathUtils {
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b) {
+        if (a == 0 || b == 0) {
+            return 0;
+        }
+        return Math.abs(a * b) / gcd(a, b);
+    }
+}
