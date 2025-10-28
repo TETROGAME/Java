c86cd756dc84b039e38369f4ac3d5a59075c7f81
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-10-28 11:15:30 +0300

    Add constructor and toString override

--- Lab/Lab 6-7/src/main/java/Reader.java
+++ Lab/Lab 6-7/src/main/java/Reader.java
@@ -1,2 +1,27 @@
 public class Reader {
+    private final String name;
+    private final String surname;
+    private final String patronymic;
+    private final String address;
+    private final String email;
+    private final String phone_number;
+    Reader(String name, String surname, String patronymic, String address, String email, String phone_number) {
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.address = address;
+        this.email = email;
+        this.phone_number = phone_number;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Name: " + name + "\n" +
+                "Surname: " + surname + "\n" +
+                "Patronymic: " + patronymic + "\n" +
+                "Address: " + address + "\n" +
+                "Email: " + email + "\n" +
+                "Phone Number: " + phone_number + "\n";
+    }
 }
