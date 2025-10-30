fe7fde42f955441e41a46d3a415a26b55776117d
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-10-30 09:16:02 +0300

    Add blacklist functionality

--- Lab/Lab 6-7/src/main/java/Administrator.java
+++ Lab/Lab 6-7/src/main/java/Administrator.java
@@ -1,2 +1,29 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Administrator {
+    private final String name;
+    private final Set<Reader> blacklist = new HashSet<>();
+
+    public Administrator(String name) {
+        this.name = name;
+    }
+
+    public void addToBlacklist(Reader reader) {
+        blacklist.add(reader);
+        System.out.printf("Administrator %s added %s %s to blacklist%n", name, reader.getSurname(), reader.getName());
+    }
+
+    public boolean isBlacklisted(Reader reader) {
+        return blacklist.contains(reader);
+    }
+
+    public void printBlacklist() {
+        System.out.println("Blacklist:");
+        if (blacklist.isEmpty()) {
+            System.out.println(" (none)");
+            return;
+        }
+        blacklist.forEach(r -> System.out.println("- " + r.getSurname() + " " + r.getName()));
+    }
 }
