2086c84d50353d116560bb7775f3e6e2abba23e0
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-10-30 08:50:03 +0300

    Add constructor and toString()

--- Lab/Lab 6-7/src/main/java/Book.java
+++ Lab/Lab 6-7/src/main/java/Book.java
@@ -1,2 +1,14 @@
 public class Book {
+    private final String title;
+    private final String author;
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Book \"" + title + "\" by " + author;
+    }
 }
