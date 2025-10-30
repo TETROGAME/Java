4953739b930849ae19e72edbdc897035a728ed8d
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-10-30 09:16:24 +0300

    Add equals and hashCode override

--- Lab/Lab 6-7/src/main/java/Book.java
+++ Lab/Lab 6-7/src/main/java/Book.java
@@ -1,11 +1,26 @@
 public class Book {
     private final String title;
     private final String author;
-    Book(String title, String author) {
+
+    public Book(String title, String author) {
         this.title = title;
         this.author = author;
     }
 
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book other)) return false;
+        return title.equals(other.title) && author.equals(other.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * title.hashCode() + author.hashCode();
+    }
 
     @Override
     public String toString() {
