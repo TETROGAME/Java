3ac7c2a5024b785b8e1004680cf734f3a5f7bfcb
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-10-30 09:23:32 +0300

    Add book search/return functionality

--- Lab/Lab 6-7/src/main/java/Catalogue.java
+++ Lab/Lab 6-7/src/main/java/Catalogue.java
@@ -1,38 +1,62 @@
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Optional;
 
 public class Catalogue {
-    private HashMap<Book, Integer> books;
+    private final HashMap<Book, Integer> books;
+
     public Catalogue(HashMap<Book, Integer> books) {
         this.books = books;
     }
 
+    public Optional<Book> searchByTitle(String title) {
+        return books.keySet().stream()
+                .filter(b -> b.getTitle().equalsIgnoreCase(title))
+                .findFirst();
+    }
+
+
+    public Optional<Book> searchByAuthor(String author) {
+        return books.keySet().stream()
+                .filter(b -> b.getAuthor().equalsIgnoreCase(author))
+                .findFirst();
+    }
 
     public void extractBook(Book book) {
-        if(books.containsKey(book)) {
-            if(books.get(book) > 0) {
+        if (books.containsKey(book)) {
+            if (books.get(book) > 0) {
                 books.replace(book, books.get(book) - 1);
-            }
-            else throw new  RuntimeException("This book is out of stock!");
-        }
-        else {
+            } else throw new RuntimeException("This book is out of stock!");
+        } else {
             throw new RuntimeException("Book not found");
         }
     }
+
+    public void returnBook(Book book) {
+        books.merge(book, 1, Integer::sum);
+    }
+
+    public void addBook(Book book, int count) {
+        books.merge(book, count, Integer::sum);
+    }
+
+    public int availableCopies(Book book) {
+        return books.getOrDefault(book, 0);
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
         sb.append("Books available:\n");
         int max_line_length = -1;
-        for(Book book : books.keySet()) {
-            if(max_line_length < book.toString().length()) {
+        for (Book book : books.keySet()) {
+            if (max_line_length < book.toString().length()) {
                 max_line_length = book.toString().length();
             }
         }
 
         sb.append("-".repeat(Math.max(0, max_line_length))).append('\n');
-        for(var book : books.entrySet()) {
+        for (var book : books.entrySet()) {
             sb
                     .append(book.getKey().toString())
                     .append(", available: ")
